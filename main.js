d13885ef1314c252f6a3cb6c631e5d5681b8237a
luizfelippealvesp <luizfelippealvesp@gmail.com>
2025-05-05 11:27:59 +0000

    exemplo 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 //Exemplo 1
 
 function mostrarAlerta() {
@@ -6,3 +7,72 @@ function mostrarAlerta() {
 }
 
 mostrarAlerta()
+*/
+
+/*
+//exemplo 2
+function somar (valorA, valorB) {
+    alert (valorA + valorB)
+  }
+  somar (20,12)
+*/
+
+//exemplo 3
+
+/*
+function mensagemBoasVindas (nome, cargo) {
+    return 'seja bem-vindo ${cargo} ${nome}'
+  }
+  let nome = prompt ("Digite o nme")
+  let cargo = prompt("Digite o cargo")
+  alert(mensagemBoasVindas(nome, cargo))
+  console.log (mensagemBoasVindas(nome,cargo))
+*/
+
+/*
+//exemplo 4
+
+ function multiplicação (numero1, numero2) {
+    return numero1 * numero2
+ }
+
+ let multiplicação1= multiplicação(6,7)
+ let multiplicação2= multiplicação(9,10)
+ let multiplicação3= multiplicação(12,25)
+ */
+
+ /*
+ //exemplo 5
+ function parOuImpar (numero) {
+    if (numero %2 === 0) {
+        return "par"
+    }
+    return "Impar"
+  }
+  console.log (parOuImpar)
+*/
+
+ /*
+ //exemplo 6 
+
+ let numero = 10
+ const button = document querySelector("button")
+
+ button.addEventListener("click", function() {
+    numero ++
+    console.log(numero)
+})
+
+ console.log(numero)
+ */
+
+/*
+//exemplo 7
+ let numero = 10
+const button = document.querySelector("button")
+button addEventListener("click",()=> {
+  numero ++
+  console.log (numero)
+})
+console.log (numero)
+*/
